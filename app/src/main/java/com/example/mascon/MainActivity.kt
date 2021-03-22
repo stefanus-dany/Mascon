8d9a5d46f01d8ae235a9b7b2216cb21b5957b89d
dany <stefanusdany12@gmail.com>
2021-03-22 22:24:18 +0700

    menambahkan fungsi login dan register menggunakan firebase

--- app/src/main/java/com/example/mascon/MainActivity.kt
+++ app/src/main/java/com/example/mascon/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.mascon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var logout: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+        logout = findViewById(R.id.btnLogout)
+        logout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        }
     }
 }
