c7d68131781168f89dd6eb320e03e72db27411ab
dany <stefanusdany12@gmail.com>
2021-03-25 09:51:35 +0700

    update login and register with realtime database

--- app/src/main/java/com/example/mascon/MainActivity.kt
+++ app/src/main/java/com/example/mascon/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.mascon.Fragment.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var logout: Button
+    private lateinit var bottomNavigation : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,33 @@ class MainActivity : AppCompatActivity() {
             FirebaseAuth.getInstance().signOut()
             startActivity(Intent(this, Login::class.java))
             finish()
+        }
+
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        val homeFragment = HomeFragment()
+        val searchFragment = SearchFragment()
+        val addFragment = AddFragment()
+        val notificationFragment = NotificationFragment()
+        val profileFragment = ProfileFragment()
+
+        makeCurrentFragment(homeFragment)
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> makeCurrentFragment(homeFragment)
+                R.id.search -> makeCurrentFragment(searchFragment)
+                R.id.addBox -> makeCurrentFragment(addFragment)
+                R.id.notification -> makeCurrentFragment(notificationFragment)
+                R.id.profile -> makeCurrentFragment(profileFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun makeCurrentFragment(fragment : Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
         }
     }
 }
